703cbb84d5cbf037fd00d6195473207184ab9fb4
Andre Fabiano Lermen <lermen.andrefabinao@gmail.com>
2019-07-19 00:02:23 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/process/processfile/ProcessFileApplication.java
@@ -0,0 +1,13 @@
+package br.com.process.processfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessFileApplication.class, args);
+	}
+
+}
