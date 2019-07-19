0da2b5d7f75cb1736517c013e648915edd1ed236
Andre Fabiano Lermen <lermen.andrefabiano@gmail.com>
2019-07-19 17:09:07 -0300

    Estrutura inicial

--- src/main/java/br/com/process/processfile/Application.java
+++ src/main/java/br/com/process/processfile/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProcessFileApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProcessFileApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
